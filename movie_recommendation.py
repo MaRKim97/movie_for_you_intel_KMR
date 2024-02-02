e56004905b299fc3245aff5e89c1fec2eef0a6fd
MaRo1 <rlaakfh97@gmail.com>
2024-02-02 15:16:02 +0900

    0202(15:15)-movie recommendation app

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -17,15 +17,35 @@ df_reviews = pd.read_csv('./cleaned_one_review.csv')
 Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
-ref_idx = 10
-print(df_reviews.iloc[ref_idx, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
-print(cosine_sim[0])
-print(len(cosine_sim))
+
+# 영화 index 이용 추천
+# ref_idx = 10
+# print(df_reviews.iloc[ref_idx, 0])
+# cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix) #코사인 유사도 값을 찾는다
+# print(cosine_sim[0])
+# print(len(cosine_sim))
+# recommendation = getRecommendation(cosine_sim)
+# print(recommendation)
+
+#keyword 이용 추천
+embedded_model = Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '애니'
+sim_world = embedded_model.wv.most_similar(keyword, topn=10)
+words = [keyword]
+for word, _ in sim_world:
+    words.append(word)
+sentence = []
+count = 10
+for word in words:
+    sentence = sentence + [word] * count
+    count -= 1
+sentence = ' '.join(sentence)
+print(sentence)
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
 recommendation = getRecommendation(cosine_sim)
+
 print(recommendation)
-
-
 
 
 
